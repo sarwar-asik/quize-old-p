a4d21db97638f46796ab584f273a7e231e5e3e65
sarwar-asik <shasik2021@gmail.com>
2022-10-11 01:28:42 +0600

    icon added

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import Course from './components/Course/Course';
-import Home from './components/homes/Home';
-import ErrorBoundary from './components/utilities/errorHandling';
-import Root from './Root';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Course from "./components/Course/Course";
+import Home from "./components/homes/Home";
+import Quize from "./components/Quiez/Quize";
+import ErrorBoundary from "./components/utilities/errorHandling";
+import Root from "./Root";
 
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Root></Root>,
-        loader:()=>fetch('fakeData.json'),
-        errorElement:<ErrorBoundary/>,
-        children:[
-            {path: '/',element:<Home></Home>},
-            {path:'course',element:<Course></Course>}
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Root></Root>,
+    loader: () => fetch("fakeData.json"),
+    errorElement: <ErrorBoundary />,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "course", element: <Course></Course> },
+      {path:'quize',element:<Quize></Quize>}
+    ],
+  },
+]);
 
 export default router;
--- src/components/singleCourse/SingleCours.js
+++ src/components/singleCourse/SingleCours.js
@@ -1,14 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import './SingleCourse.css'
+import './SingleCourse.css';
+import { faCheckSquare, faCoffee,faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const SingleCours = ({course}) => {
     const {name,id,logo,total} =course;
     return (
-        <div className='single-course bg-secondary text-center  text-light p-5 m-2 rounded'>
+        <div className='single-course bg-secondary text-center  text-light p-5 m-2 rounded shadow-lg'>
             <h1>Test yourself with<br></br> <i>{name}</i></h1>
             <img className='course-img' src={logo} alt="" />
             <h4>Total Number {total}</h4>
-        <button className='btn btn-warning btn-outline-danger fs-3 text-muted border border-none'>Go Test </button>
+      
+      
+      <button className='btn btn-warning btn-outline-info fs-3 text-muted border border-none'> Start Test <FontAwesomeIcon icon={faArrowRightArrowLeft}/></button>
+
+
             
         </div>
     );
