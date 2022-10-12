19397478814bc433d292eb8749affaf611a35898
sarwar-asik <shasik2021@gmail.com>
2022-10-12 11:24:41 +0600

    complete project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,36 +4,72 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <div className="tittle my-2">
-        <h2>Developer Test</h2>
-      </div>
+    <div classNameName="header-container">
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid ">
+          <div classNameName="me-5">
+            <h2>Developer Test</h2>
+          </div>
 
-      <nav className="nav-link">
-        <NavLink
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-          to={"/"}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-          to={"chart"}
-        >
-          Avarage chart
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-          to={"blogs"}
-        >
-          Blogs
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-          to={"about"}
-        >
-          About
-        </NavLink>
+          <div className="">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse mt-3" id="navbarNavDropdown">
+              <div className="navbar-nav">
+                <p className="nav-item">
+                  <NavLink
+                    classNameName={({ isActive }) =>
+                      isActive ? "active" : undefined
+                    }
+                    to={"/"}
+                  >
+                    Home
+                  </NavLink>
+                </p>
+                <p className="nav-item">
+                  <NavLink
+                    classNameName={({ isActive }) =>
+                      isActive ? "active" : undefined
+                    }
+                    to={"chart"}
+                  >
+                    Avarage chart
+                  </NavLink>
+                </p>
+                <p className="nav-item">
+                  <NavLink
+                    classNameName={({ isActive }) =>
+                      isActive ? "active" : undefined
+                    }
+                    to={"blogs"}
+                  >
+                    Blogs
+                  </NavLink>
+                </p>
+                <p className="nav-item">
+                  <NavLink
+                    classNameName={({ isActive }) =>
+                      isActive ? "active" : undefined
+                    }
+                    to={"about"}
+                  >
+                    About
+                  </NavLink>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
     </div>
   );
